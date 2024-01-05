d2ce627edb4b84a564ef9438125a158f8e234782
buddhika4u <buddhika4u98@gmail.com>
2024-01-05 18:14:30 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+require("dotenv").config();
+const app = express();
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    // useCreateIndex: true, // no longer supported in the MongoDB Node.js driver version 4.0 and above
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false, // no longer supported in the MongoDB Node.js driver version 4.0 and above
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => console.log('MongoDB Connection Success! 👍'));
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port number: ${PORT}`);
+})
